ce732802910c1bebc199eceef2e2ae65901111e7
Alexis2694 <133776202+al222210707@users.noreply.github.com>
2024-04-08 13:20:22 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
+// Configuración de la conexión a la base de datos
+const connection = mysql.createConnection({
   host: '162.241.62.191',
   user: 'cuervoso_smar',
   password: 'gQz3fa47ebHgGt7',
   database: 'cuervoso_luz'
 });
 
-db.connect((err) => {
+// Conectar a la base de datos
+connection.connect((err) => {
   if (err) {
-    
+    console.error('Error al conectar a la base de datos:', err);
+    return;
   }
-  console.log('MySQL connected');
+  console.log('Conexión exitosa a la base de datos');
 });
 
-module.exports = db;
+module.exports = connection;
