cb6789baa4ca1263ba42d87188d964db908bc06f
Alexis2694 <133776202+al222210707@users.noreply.github.com>
2024-04-08 13:35:17 -0600

    Update routes.js

--- routes.js
+++ routes.js
@@ -28,16 +28,4 @@ router.get('/mediciones', (req, res) => {
   });
 });
 
-// Ruta para obtener todas las mediciones de consumo de un usuario por su ID
-router.get('/users/:id/mediciones', (req, res) => {
-  const id = req.params.id;
-  db.query('SELECT * FROM consumptiondata WHERE id_user = ?', id, (err, rows) => {
-    if (err) {
-      console.error('Error al obtener mediciones de consumo:', err);
-      res.status(500).json({ error: 'Error interno del servidor' });
-      return;
-    }
-    res.json({ mediciones: rows });
-  });
-});
-
+module.exports = router;
