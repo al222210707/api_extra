0c886524e4775be2ae7b11a4fc45ea93e7becb2e
Alexis2694 <133776202+al222210707@users.noreply.github.com>
2024-04-09 12:09:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 
-const db = mysql.createConnection({
-  host: 'srv1247.hstgr.io',
-  user: 'u630845697_luz',
-  password: '@RamirezLzrga41',
-  database: 'u630845697_smartpower'
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor API corriendo en http://localhost:${PORT}`); 
 });
-
-db.connect((err) => {
-  if (err) {
-  }
-  console.log('MySQL connected');
-});
-
-module.exports = db;
