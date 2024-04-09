7a335deae58bbdc7d640ea8eb310aa81c8066128
Alexis2694 <133776202+al222210707@users.noreply.github.com>
2024-04-09 13:10:39 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: '193.203.166.112',
+  host: 'srv1247.hstgr.io',
   user: 'u630845697_luz',
   password: '@Ye738292o9rjfs',
   database: 'u630845697_smartpower'
