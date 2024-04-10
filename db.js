7fc6ee41a1947dbfef5553c6c216b8b1e3f568fd
Alexis2694 <133776202+al222210707@users.noreply.github.com>
2024-04-09 19:34:32 -0600

    Update db.js

--- db.js
+++ db.js
@@ -6,13 +6,20 @@ const db = mysql.createConnection({
   password: '@Ye738292o9rjfs',
   database: 'u630845697_smartpower'
 });
+
 db.connect((err) => {
   if (err) {
     console.error('Error al conectar con la base de datos:', err);
-    // Manejar el error apropiadamente, por ejemplo, lanzar una excepción o emitir un evento de error
+    // Aquí puedes manejar el error de conexión, lanzar una excepción o emitir un evento de error
   } else {
     console.log('MySQL connected');
   }
+});
+
+// Manejo de errores para consultas
+db.on('error', (err) => {
+  console.error('Error en la conexión a la base de datos:', err);
+  // Aquí puedes manejar el error de la conexión de base de datos
 });
 
 module.exports = db;
