fbc4268d979bf5b197737e7a46e51aacf77c67dd
Alexis2694 <133776202+al222210707@users.noreply.github.com>
2024-04-09 19:47:00 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,25 +1,34 @@
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: 'srv1247.hstgr.io',
-  user: 'u630845697_luz',
-  password: '@Ye738292o9rjfs',
-  database: 'u630845697_smartpower'
-});
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.error('Error al conectar con la base de datos:', err);
-    // Aquí puedes manejar el error de conexión, lanzar una excepción o emitir un evento de error
-  } else {
-    console.log('MySQL connected');
-  }
-});
+function handleDisconnect() {
+  db = mysql.createConnection({
+    host: 'srv1247.hstgr.io',
+    user: 'u630845697_luz',
+    password: '@Ye738292o9rjfs',
+    database: 'u630845697_smartpower'
+  });
 
-// Manejo de errores para consultas
-db.on('error', (err) => {
-  console.error('Error en la conexión a la base de datos:', err);
-  // Aquí puedes manejar el error de la conexión de base de datos
-});
+  db.connect(function(err) {
+    if (err) {
+      console.error('Error en la conexión a la base de datos:', err);
+      setTimeout(handleDisconnect, 2000); // Intentar la reconexión después de 2 segundos
+    } else {
+      console.log('MySQL connected');
+    }
+  });
+
+  db.on('error', function(err) {
+    console.error('Error de base de datos:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); // Reconectar si la conexión se perdió
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = db;
